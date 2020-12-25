8b802154237c9718eeba668081c8c5ae3b55356a
Łukasz Czapraga <lczapraga@gmail.com>
2020-12-25 23:15:33 +0100

    Initial commit

---
+++ src/main/java/com/leggeart/legge/LeggeApplication.java
@@ -0,0 +1,13 @@
+package com.leggeart.legge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeggeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeggeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leggeart/legge/LeggeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leggeart.legge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeggeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
