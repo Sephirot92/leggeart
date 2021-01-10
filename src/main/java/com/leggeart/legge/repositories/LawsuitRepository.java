bbc627d71eae6aadce8d52507d1b5f151ae8b2c2
Łukasz Czapraga <lczapraga@gmail.com>
2021-01-10 23:21:26 +0100

    00003 Added Service, Service Implementation class and basic Lawsuit class in version 0.0.1.

---
+++ src/main/java/com/leggeart/legge/repositories/LawsuitRepository.java
@@ -0,0 +1,26 @@
+package com.leggeart.legge.repositories;
+
+import com.leggeart.legge.entities.Lawsuit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ Repository class to handle Lawsuit class.
+
+ @version 0.0.1
+ @author LC
+ */
+@Transactional
+@Repository
+public interface LawsuitRepository extends JpaRepository<Lawsuit, Long> {
+
+    @Override
+    List<Lawsuit> findAll();
+
+    @Override
+    Optional<Lawsuit> findById(Long id);
+}
